826a91a63b4c205df64b3a2fd30aef531f7d6af9
Noel Proctor <noelproctor@hotmail.co.uk>
2024-11-21 06:58:51 +0000

    pushing databaseEample
    pushing databaseEample

---
+++ DatabaseExample/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO widgets (id, name, purpose) VALUES
+(1, 'WIDGET A', 'Used for testing purposes.'),
+(2, 'WIDGET B', 'Designed For Entertainment.'),
+(3, 'WIDGET C', 'Enhances Productivity.');
